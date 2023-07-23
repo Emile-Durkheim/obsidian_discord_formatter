af9f9822002e23de0d2a43662b2aabee41c65bd8
Emile Durkheim <github@andretsch.at>
2023-07-23 18:17:44 +0200

    now pastes markdown into editor

--- discordMessageToMarkdown.ts
+++ discordMessageToMarkdown.ts
@@ -1,48 +1,86 @@
-import { log, writeDocumentToFile } from 'utils';
+import { log } from 'utils';
 
 export default function discordMessageToMarkdown(event: ClipboardEvent): string | undefined {
     // Check that there is data in clipboardEvent
-	if (!event.clipboardData){
-		log("clipboardData empty")
-		return;
-	}
+    if (!event.clipboardData){
+        log("clipboardData empty")
+        return;
+    }
     
     // Parse clipboardEvent into a Document if possible
-    const messageDOM: Document | null = clipboardToDocument(event.clipboardData);
-    if(messageDOM === null){
+    const messageDOM = clipboardToDocument(event.clipboardData);
+    if(!messageDOM){
         log("No HTML in paste");
-		return;
-    }
-
-    // Ensure it's a Discord message
-    log('isDiscordMessage: ', isDiscordMessage(messageDOM));
-    if(!isDiscordMessage(messageDOM)){
         return;
     }
 
-
-}
-
-
-function clipboardToDocument(clipboardData: DataTransfer): Document | null {
-	const rawHTML: string | undefined = clipboardData.getData('text/html');
-    
-    // Ensure rawHTML is defined
-    if(!rawHTML){
-        return null;
+    // Ensure it's a Discord message
+    if(!isDiscordMessage(messageDOM)){
+        log("Not a Discord message");
+        return;
     }
 
-	const parser = new DOMParser();
+    // Parse message to markdown
 
-	const messageDOM: Document = parser.parseFromString(rawHTML, 'text/html');
+    // That children exist was already ensured by isDiscordMessage()
+    // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+    const messageDivs: Element[] = Array.from(messageDOM.body.firstElementChild!.children!);
+    const markdown: string[] = [];
 
+    for(const messageDiv of messageDivs)
+    {
+        // Parse the header that contains username/time if it exists on the message
+        const messageHeader = messageDiv.querySelector("h3");
 
-	return messageDOM;
+        if(messageHeader){
+            const username = messageHeader.querySelector("span > span")?.textContent;
+            const time = messageHeader.querySelector("time")?.textContent;
+            
+            if(!(username && time)){
+                throw new Error("Couldn't get username or time.");
+            }
+
+            // Insert empty line between different user's messages
+            if(markdown.length != 0){
+                markdown.push('\n');
+            }
+            markdown.push(`>**${username}${time}**`);
+        }
+        
+        
+        // Parse the message
+        // no-non-null-assertion: Assured to be non-null by HTML structure of Discord message
+        // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+        const messageFragments: string[] = [];
+        const messageContentSpans = messageDiv.querySelector("div[id*='message-content']")?.children;
+
+        if(messageContentSpans === undefined){
+            log("Contains no message: ", messageDiv);
+            continue;
+        }
+
+        for(const messageContentSpan of Array.from(messageContentSpans)){
+            messageFragments.push(messageContentSpan.innerHTML);
+        }
+
+        // Push the message to our markdown
+        markdown.push(`>${messageFragments.join('')}`);
+    }
+
+    log(markdown.join('\n'));
+    return markdown.join('\n');
 }
 
 
 function isDiscordMessage(doc: Document){
-	if(doc.body?.firstChild?.nodeName != 'OL'){
+    // Checks whether the message has a few hallmarks of a Discord DOM, i.e.:
+    // First child of body is an OL with the properties:
+    // class="scrollInner-xxxxx", dataset-list-id="chat-messages", and an <li> child
+    //
+    // Should probably be made more robust in the future; could imagine this would catch Slack messages too... 
+
+	if(doc.body?.firstChild?.nodeName != 'OL')
+    {
 		return false;
 	}
     
@@ -50,9 +88,23 @@ function isDiscordMessage(doc: Document){
 	// eslint-disable-next-line @typescript-eslint/no-non-null-assertion
 	const ol: HTMLElement = doc.body.firstElementChild! as HTMLElement;
 
-	return (
+    return (
 			ol.dataset.listId == "chat-messages" && 
-            /scrollerInner/.test(ol.className) &&
+            /scrollerInner-.{6}/.test(ol.className) &&
             ol.firstElementChild?.nodeName == 'LI'
-	);
+           )
+}
+
+
+function clipboardToDocument(clipboardData: DataTransfer): Document | undefined {
+	const rawHTML: string | undefined = clipboardData.getData('text/html');
+    
+    if(!rawHTML){
+        return;
+    }
+
+	const parser = new DOMParser();
+	const messageDOM: Document = parser.parseFromString(rawHTML, 'text/html');
+
+	return messageDOM;
 }
--- main.ts
+++ main.ts
@@ -1,5 +1,6 @@
-import { Plugin } from 'obsidian';
+import { Editor, MarkdownView, Plugin } from 'obsidian';
 import discordMessageToMarkdown from 'discordMessageToMarkdown';
+import { log, writeStringToFile } from 'utils';
 
 export const SETTINGS = {
 	debug: true
@@ -9,25 +10,37 @@ export default class DiscordFormatter extends Plugin {
 	settings = SETTINGS
 
 	async onload() {
-		// A test command
 		this.addCommand({
-			id: 'test-command',
-			name: 'hello world',
-			callback: () => {
-				console.log("Test command");
+			id: "test-command",
+			name: "Test command",
+			editorCallback: (editor: Editor) => {
+				editor.replaceSelection("Test")
 			}
 		})
-
-
-		// Define behaviour on paste
-		this.app.workspace.on('editor-paste', discordMessageToMarkdown);
 		
+		// Define behaviour on paste
+		this.app.workspace.on('editor-paste', (event) => this.pasteDiscordMessageAsMarkdown(event))
+
 		// Debug
-		this.app.workspace.on('editor-paste', (clipboardEvent) => {console.log(clipboardEvent.clipboardData?.getData('text/html'))});
+		this.app.workspace.on('editor-paste', (clipboardEvent) => {writeStringToFile(clipboardEvent.clipboardData?.getData('text/html'))});
 	}
 
 
 	onunload() {
-		this.app.workspace.off('editor-paste', discordMessageToMarkdown);
+		this.app.workspace.off('editor-paste', (event) => this.pasteDiscordMessageAsMarkdown(event));
+		this.app.workspace.off('editor-paste', (clipboardEvent) => {writeStringToFile(clipboardEvent.clipboardData?.getData('text/html'))});
+	}
+
+
+	pasteDiscordMessageAsMarkdown(event: ClipboardEvent){
+		const discordMarkdown = discordMessageToMarkdown(event);
+		log(discordMarkdown);
+		
+		if(typeof discordMarkdown == "string"){
+			event.preventDefault();
+
+			const view = this.app.workspace.getActiveViewOfType(MarkdownView);
+			if(view?.editor) view.editor.replaceSelection(discordMarkdown);
+		}
 	}
 }
