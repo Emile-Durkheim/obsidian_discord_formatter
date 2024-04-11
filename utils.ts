60601ab26af3c07535fdc560cacbba5c1fa05c28
Emile Durkheim <98767867+Emile-Durkheim@users.noreply.github.com>
2024-04-11 14:17:26 +0200

    now pastes markdown into editor

--- utils.ts
+++ utils.ts
@@ -10,6 +10,20 @@ export function writeDocumentToFile(doc: Document): void {
     const str = serializer.serializeToString(doc);
 
     for(let i=0; i < 30; i++){
+        if(!fs.existsSync(`~/Documents/${i}.html`)){
+            fs.writeFile(`~/Documents/${i}.html`, str, () => {});
+            return;
+        }
+    }
+}
+
+export function writeStringToFile(str: string | undefined){
+    if(typeof str == "undefined"){
+        log("string undefined")
+        return;
+    }
+
+    for(let i=0; i < 30; i++){
         if(!fs.existsSync(`${i}.html`)){
             fs.writeFile(`${i}.html`, str, () => {});
             return;
@@ -18,6 +32,7 @@ export function writeDocumentToFile(doc: Document): void {
 }
 
 
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
 export function log(...params: any[]): void{
 	if(SETTINGS.debug){
         console.log(...params);
