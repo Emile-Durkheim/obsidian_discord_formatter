9e64267037757306af23da29216be93a003131d1
Emile Durkheim <98767867+Emile-Durkheim@users.noreply.github.com>
2024-05-17 12:15:52 +0200

    added drag-and-drop support

--- src/main.ts
+++ src/main.ts
@@ -9,6 +9,7 @@ import { IDiscordFormatterSettings } from "./settings";
 export default class DiscordFormatter extends Plugin {
 	settings: IDiscordFormatterSettings
 	pasteMessageHandler: (event: ClipboardEvent) => void;
+	dragStartHandler: (event: DragEvent) => void;
 
 	async onload() {
 		this.init();
@@ -19,10 +20,12 @@ export default class DiscordFormatter extends Plugin {
 
 		// Settings tab
 		this.addSettingTab(new SettingsTab(this.app, this));
-		
+
 		// Define behaviour on paste
 		this.pasteMessageHandler = this.pasteMessage.bind(this);
+		this.dragStartHandler = this.dragStart.bind(this);
 		this.app.workspace.on('editor-paste', this.pasteMessageHandler)
+		this.app.workspace.on("editor-drop", this.dragStartHandler)
 	}
 
 
@@ -39,6 +42,24 @@ export default class DiscordFormatter extends Plugin {
 		this.saveData(this.settings);
 	}
 
+	dragStart(event: DragEvent){
+		const rawHTML = event.dataTransfer?.getData('text/html');
+		const view = this.app.workspace.getActiveViewOfType(MarkdownView);
+
+		// Return if no html in clipboard or no editor open => if no Discord message to paste
+		if(!(rawHTML && view?.editor)){
+			return;
+		}
+
+		const conversation = DiscordConversation.fromRawHTML(rawHTML);
+		if(conversation.messages.length == 0){
+			return;
+		}
+
+		event.preventDefault();
+		view.editor.replaceSelection(conversation.toMarkdown(this.settings));
+
+	}
 
 	pasteMessage(event: ClipboardEvent){
 		const rawHTML = event.clipboardData?.getData('text/html');
@@ -48,7 +69,7 @@ export default class DiscordFormatter extends Plugin {
 		if(!(rawHTML && view?.editor)){
 			return;
 		}
-		
+
 		const conversation = DiscordConversation.fromRawHTML(rawHTML);
 		if(conversation.messages.length == 0){
 			return;
@@ -58,4 +79,3 @@ export default class DiscordFormatter extends Plugin {
 		view.editor.replaceSelection(conversation.toMarkdown(this.settings));
 	}
 }
-
